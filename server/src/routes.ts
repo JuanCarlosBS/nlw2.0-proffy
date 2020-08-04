2792ee584881f8a0862b12542ce5f935284f1868
juancarlosbs <juanrov_@hotmail.com>
2020-08-04 19:38:49 -0300

    create connections controller and ready second day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController'
+import ConnectionController from './controllers/ConnectionsController';
 
 const routes = express.Router()
-const ClassesControllers = new ClassesController();
+const classesControllers = new ClassesController();
+const connectionsControllers = new ConnectionController
 
-routes.get('/classes', ClassesControllers.index)
-routes.post('/classes', ClassesControllers.create)
+routes.get('/classes', classesControllers.index)
+routes.post('/classes', classesControllers.create)
+
+routes.get('/connections', connectionsControllers.index)
+routes.post('/connections', connectionsControllers.create)
 
 export default routes
